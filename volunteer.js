73aec3365801283ccb6815f598aba7448a209fff
karla-ravin <41647831+karla-ravin@users.noreply.github.com>
2018-08-14 23:01:58 -0400

    Add files via upload

--- volunteer.js
+++ volunteer.js
@@ -2,6 +2,6 @@ function run() {
   var loc = document.getElementById("location").value;
   var serv = document.getElementById("services").value;
   var link = document.getElementById("results");
-  var path = "https://karla-ravin.github.io/gwcfinal/"
+  var path = "https://karla-ravin.github.io/uh/"
   link.href = path+"results.html"+"?"+"location="+loc+"&services="+serv;
 }
