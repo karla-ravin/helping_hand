687cc8bdfb2b620d3dcb2c81841d656a2aad4d27
karla-ravin <41647831+karla-ravin@users.noreply.github.com>
2018-08-17 18:30:34 -0400

    Add files via upload

--- volunteer.js
+++ volunteer.js
@@ -2,6 +2,6 @@ function run() {
   var loc = document.getElementById("location").value;
   var serv = document.getElementById("services").value;
   var link = document.getElementById("results");
-  var path = "https://karla-ravin.github.io/uh/"
+  var path = "https://karla-ravin.github.io/helping_hand/"
   link.href = path+"results.html"+"?"+"location="+loc+"&services="+serv;
 }
